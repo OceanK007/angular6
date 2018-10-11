446630e5c8b97643ce1eac92995b4e77384c4d78
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-10-11 17:35:15 +0530

    Adding changes

---
+++ src/app/shared/ingredient.mode.ts
@@ -0,0 +1,14 @@
+export class Ingredient
+{
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number)
+    // {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // Shortcut of above code
+    constructor(public name: string, public amount: number) {}
+}
