691752c0f6b06ade66da507b5d218c6a9c408b50
Kanhaiya Thakur <oceank007@gmail.com>
2018-11-13 02:25:27 +0530

    Adding modularization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.services';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesService } from './recipes/recipes-services';
 import { DataStorageService } from './shared/data-storage.service';
 import { HttpModule } from '@angular/http';
@@ -22,30 +12,29 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { FirebaseAuthService } from './auth/firebase.auth.service';
 import { FirebaseAuthGuardService } from './auth/firebase-auth-guard.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    // BrowserModule is superset of CommonModule. 
+    // So add CommonModule in other(feature) modules
+    // And BrowserModule must be only in app.module.ts
+    BrowserModule,  
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    RecipesModule,
+    SharedModule,
+    ShoppingListModule
   ],
+  // Even if you doing modularization, if you want a service to be available for whole application, provide it in app.module.ts
   providers: [ShoppingListService, RecipesService, DataStorageService, FirebaseAuthService, FirebaseAuthGuardService],
   bootstrap: [AppComponent]
 })
